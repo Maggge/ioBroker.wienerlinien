4d558a0600aa391828b401ebe090381ed03b54e3
Maggge <magge.mair@gmail.com>
2020-07-19 11:58:04 +0200

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'wienerlinien adapter settings': {
+        'en': 'Adapter settings for wienerlinien',
+        'de': 'Adaptereinstellungen für wienerlinien',
+        'ru': 'Настройки адаптера для wienerlinien',
+        'pt': 'Configurações do adaptador para wienerlinien',
+        'nl': 'Adapterinstellingen voor wienerlinien',
+        'fr': "Paramètres d'adaptateur pour wienerlinien",
+        'it': "Impostazioni dell'adattatore per wienerlinien",
+        'es': 'Ajustes del adaptador para wienerlinien',
+        'pl': 'Ustawienia adaptera dla wienerlinien',
+        'zh-cn': 'wienerlinien的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
